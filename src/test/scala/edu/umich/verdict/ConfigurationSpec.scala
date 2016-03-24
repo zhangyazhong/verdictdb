957ca82fd883085282af6b5a3ddbee8a684faf92
shahab <ahmadshahab.t@gmail.com>
2016-03-23 20:36:59 -0400

    more unit test for Configuration

--- src/test/scala/edu/umich/verdict/ConfigurationSpec.scala
+++ src/test/scala/edu/umich/verdict/ConfigurationSpec.scala
@@ -2,12 +2,10 @@ package edu.umich.verdict
 
 import java.io.File
 
-import org.scalatest._
-
 class ConfigurationSpec extends VerdictFlatSpec {
   val conf = new Configuration(new File(this.getClass.getClassLoader.getResource("testconfig.conf").getFile))
 
-  "Configuration" should "have default values" in {
+  "Configuration" should "have the default values" in {
     assert(conf.get("bootstrap") != null)
   }
 
@@ -33,4 +31,54 @@ class ConfigurationSpec extends VerdictFlatSpec {
       conf.get("testkey2")
     }
   }
+
+  it should "work well with numerical values" in {
+    conf.set("int", "12")
+    conf.set("double", "12.5")
+    assertResult(12){
+      conf.getInt("int")
+    }
+    assertResult(12.5){
+      conf.getDouble("double")
+    }
+  }
+
+  it should "work well with boolean values" in {
+    conf.set("bool1", "False")
+    conf.set("bool2", "OFF")
+    conf.set("bool3", "no")
+    conf.set("bool4", "TRUE")
+    conf.set("bool5", "ON")
+    conf.set("bool6", "yeS")
+    assertResult(false){
+      conf.getBoolean("bool1")
+    }
+    assertResult(false){
+      conf.getBoolean("bool2")
+    }
+    assertResult(false){
+      conf.getBoolean("bool3")
+    }
+    assertResult(true){
+      conf.getBoolean("bool4")
+    }
+    assertResult(true){
+      conf.getBoolean("bool5")
+    }
+    assertResult(true){
+      conf.getBoolean("bool6")
+    }
+  }
+
+  it should "work well with percentages" in {
+    conf.set("percent1", ".546")
+    conf.set("percent2", "3.12%")
+    assertResult(.546){
+      conf.getPercent("percent1")
+    }
+    assertResult(3.12/100){
+      conf.getPercent("percent2")
+    }
+  }
+
 }
