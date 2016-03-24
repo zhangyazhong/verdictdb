11c3505d09395b3630d4a79c783cec74f64de105
shahab <ahmadshahab.t@gmail.com>
2016-03-24 15:16:59 -0400

    Configuration tests with matchers

--- src/test/scala/edu/umich/verdict/ConfigurationSpec.scala
+++ src/test/scala/edu/umich/verdict/ConfigurationSpec.scala
@@ -2,45 +2,39 @@ package edu.umich.verdict
 
 import java.io.File
 
-class ConfigurationSpec extends VerdictFlatSpec {
-  val conf = new Configuration(new File(this.getClass.getClassLoader.getResource("testconfig.conf").getFile))
+import org.scalatest.BeforeAndAfterAll
+
+class ConfigurationSpec extends VerdictFlatSpec with BeforeAndAfterAll {
+  var conf: Configuration = _
+
+  override def beforeAll() {
+    conf = new Configuration(new File(this.getClass.getClassLoader.getResource("testconfig.conf").getFile))
+  }
 
   "Configuration" should "have the default values" in {
-    assert(conf.get("bootstrap") != null)
+    conf.get("bootstrap") should not be null
   }
 
   it should "have values from file" in {
-    assertResult("Value1"){
-      conf.get("key1")
-    }
-    assertResult("other"){
-      conf.get("bootstrap.method")
-    }
+    conf.get("key1") shouldBe "Value1"
+    conf.get("bootstrap.method") shouldBe "other"
   }
 
   it should "set values" in {
     conf.set("testKey", "testValue")
-    assertResult("testValue"){
-      conf.get("testKey")
-    }
+    conf.get("testKey") shouldBe "testValue"
   }
 
   it should "ignore key letter case" in {
     conf.set("TESTKEY2", "testValue3")
-    assertResult("testValue3"){
-      conf.get("testkey2")
-    }
+    conf.get("testkey2") shouldBe "testValue3"
   }
 
   it should "work well with numerical values" in {
     conf.set("int", "12")
     conf.set("double", "12.5")
-    assertResult(12){
-      conf.getInt("int")
-    }
-    assertResult(12.5){
-      conf.getDouble("double")
-    }
+    conf.getInt("int") shouldBe 12
+    conf.getDouble("double") shouldBe 12.5
   }
 
   it should "work well with boolean values" in {
@@ -50,35 +44,19 @@ class ConfigurationSpec extends VerdictFlatSpec {
     conf.set("bool4", "TRUE")
     conf.set("bool5", "ON")
     conf.set("bool6", "yeS")
-    assertResult(false){
-      conf.getBoolean("bool1")
-    }
-    assertResult(false){
-      conf.getBoolean("bool2")
-    }
-    assertResult(false){
-      conf.getBoolean("bool3")
-    }
-    assertResult(true){
-      conf.getBoolean("bool4")
-    }
-    assertResult(true){
-      conf.getBoolean("bool5")
-    }
-    assertResult(true){
-      conf.getBoolean("bool6")
-    }
+    conf.getBoolean("bool1") shouldBe false
+    conf.getBoolean("bool2") shouldBe false
+    conf.getBoolean("bool3") shouldBe false
+    conf.getBoolean("bool4") shouldBe true
+    conf.getBoolean("bool5") shouldBe true
+    conf.getBoolean("bool6") shouldBe true
   }
 
   it should "work well with percentages" in {
     conf.set("percent1", ".546")
     conf.set("percent2", "3.12%")
-    assertResult(.546){
-      conf.getPercent("percent1")
-    }
-    assertResult(3.12/100){
-      conf.getPercent("percent2")
-    }
+    conf.getPercent("percent1") shouldBe .546
+    conf.getPercent("percent2") shouldBe 3.12 / 100
   }
 
 }
