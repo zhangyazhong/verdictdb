6f53347084b6050fb06f2b8e599249b515f32316
shahab <ahmadshahab.t@gmail.com>
2016-04-20 17:21:53 -0400

    save results to file

--- src/test/scala/edu/umich/verdict/expr/ErrorEstimationAccuracy.scala
+++ src/test/scala/edu/umich/verdict/expr/ErrorEstimationAccuracy.scala
@@ -2,6 +2,7 @@ package edu.umich.verdict.expr
 
 import java.io.{PrintWriter, File}
 import java.sql.ResultSet
+import java.util.Calendar
 
 import edu.umich.verdict.Configuration
 import edu.umich.verdict.cli.ResultWriter
@@ -11,7 +12,7 @@ import edu.umich.verdict.transformation.Parser
 import scala.io.Source
 
 class ErrorEstimationAccuracy() {
-  var conf: Configuration = null
+  var conf = new Configuration(new File(this.getClass.getClassLoader.getResource("expr/config.conf").getFile))
   var connector: DbConnector = null
   var nSamples = 1000
   var sampleSize = 0.01
@@ -33,6 +34,8 @@ class ErrorEstimationAccuracy() {
     """.stripMargin)
   var exacts: Array[Array[Double]] = null
   var approximates: Array[Array[Array[ApproxResult]]] = null
+  var dir: String = null
+
 
   def execute(q: String): ResultSet = {
     Parser.parse(q).run(conf, connector)
@@ -59,16 +62,17 @@ class ErrorEstimationAccuracy() {
   def runExacts() = {
     execute("set approximation = off")
     queries.zipWithIndex.foreach(q => {
-      val pw = new PrintWriter(new File(s"error-test/${q._2}/exact"))
+      val pw = new PrintWriter(new File(s"error-test/$dir/${q._2}/exact"))
       ResultWriter.writeResultSet(pw, execute(q._1))
       pw.close()
     })
   }
 
   def runApproximates() = {
+    execute(s"set bootstrap.sample_size = $sampleSize%")
     execute("set approximation = on")
     queries.zipWithIndex.foreach(q => {
-      val pw = new PrintWriter(new File(s"error-test/${q._2}/approx"))
+      val pw = new PrintWriter(new File(s"error-test/$dir/${q._2}/approx"))
       for (i <- 1 to nSamples) {
         execute(s"set bootstrap.fixed_sample = ${sampleName(i)}")
         ResultWriter.writeResultSet(pw, execute(q._1))
@@ -79,7 +83,7 @@ class ErrorEstimationAccuracy() {
 
   def loadExacts() = {
     exacts = queries.indices.map(i => {
-      Source.fromFile(s"error-test/$i/exact").getLines().toArray.apply(2).split("\\|").map(_.trim.toDouble)
+      Source.fromFile(s"error-test/$dir/$i/exact").getLines().toArray.apply(2).split("\\|").map(_.trim.toDouble)
     }).toArray
     exacts
   }
@@ -89,7 +93,7 @@ class ErrorEstimationAccuracy() {
       loadExacts()
     approximates = queries.indices.map(q => {
       val nCols = exacts(q).length
-      var lines = Source.fromFile(s"error-test/$q/approx").getLines().toArray
+      var lines = Source.fromFile(s"error-test/$dir/$q/approx").getLines().toArray
       lines = lines.indices.filter(_ % 3 == 2).map(lines).toArray
       (0 until nCols).map(i => {
         lines.map(line => {
@@ -120,23 +124,25 @@ class ErrorEstimationAccuracy() {
     }
     queries.indices.foreach(q => {
       println(s"Variance error for query $q:")
+      val pw = new PrintWriter(new File(s"error-test/$dir/$q/report-variance-error"))
       approximates(q).indices.foreach(col => {
         val colVals = approximates(q)(col)
         val exact = variance(colVals)
         val errorAvg = colVals.map(app => math.abs((app.variance - exact) / exact)).sum / colVals.length
         println(s"column $col: ${100 * errorAvg}%")
+        pw.println(s"column $col: ${100 * errorAvg}%")
       })
+      pw.close()
     })
   }
 
   def connect() = {
-    conf = new Configuration(new File(this.getClass.getClassLoader.getResource("expr/config.conf").getFile))
     connector = DbConnector.createConnector(conf)
   }
 
   def printConfidenceIntervalError(method: String = "diff") = {
     def exactConfidenceInterval(exactVal: Double, apps: Array[ApproxResult]) = {
-      val confidence = .95
+      val confidence = if (conf != null) conf.getPercent("bootstrap.confidence") else .95
       val trials: Int = apps.length
       val sortedVals = apps.map(_.value).sortBy(x => math.abs(exactVal - x))
       val bound = sortedVals(math.ceil(trials * confidence - 1).asInstanceOf[Int])
@@ -147,22 +153,44 @@ class ErrorEstimationAccuracy() {
     def confidenceIntervalError(exact: (Double, Double), approx: (Double, Double)) = {
       if (method == "diff")
         (math.abs(exact._1 - approx._1) + math.abs(exact._2 - approx._2)) / (exact._2 - exact._1)
-      else
+      else if (method == "abs-width")
         math.abs((exact._2 - exact._1) - (approx._2 - approx._1)) / (exact._2 - exact._1)
+      else if (method == "width")
+        ((approx._2 - approx._1) - (exact._2 - exact._1)) / (exact._2 - exact._1)
+      else
+        0.0
     }
 
     queries.indices.foreach(q => {
-      println(s"Confidence interval error for query $q:")
+      println(s"Confidence interval error for query $q ($method):")
+      val pw = new PrintWriter(new File(s"error-test/$dir/$q/report-confidence-interval-error-$method"))
       approximates(q).indices.foreach(col => {
         val colVals = approximates(q)(col)
         val exact = exactConfidenceInterval(exacts(q)(col), colVals)
         val errorAvg = colVals.map(app => confidenceIntervalError(exact, (app.ciLow, app.ciHigh))).sum / colVals.length
         println(s"column $col: ${100 * errorAvg}%")
+        pw.println(s"column $col: ${100 * errorAvg}%")
       })
+      pw.close()
     })
   }
 
-  def main(args: Array[String]) {
+  def printEstimatedErrors() = {
+    queries.indices.foreach(q => {
+      println(s"Average estimated error for query $q:")
+      val pw = new PrintWriter(new File(s"error-test/$dir/$q/report-estimated-error"))
+      approximates(q).indices.foreach(col => {
+        val colVals = approximates(q)(col)
+        val avg = colVals.map(app => math.abs(app.error / app.value)).sum / colVals.length
+        println(s"column $col: ${100 * avg}%")
+        pw.println(s"column $col: ${100 * avg}%")
+      })
+      pw.close()
+    })
+  }
+
+  def run() {
+    println("Connecting ...")
     connect()
 
     createDirs()
@@ -170,15 +198,17 @@ class ErrorEstimationAccuracy() {
     println("Running Exacts ...")
     runExacts()
 
-    println("Creating Samples ...")
-    createSamples()
+    //    println("Creating Samples ...")
+    //    createSamples()
 
-    println("Creating Approximates ...")
-    execute(s"set bootstrap.sample_size = $sampleSize%")
+    println("Running Approximates ...")
     runApproximates()
 
     loadExacts()
     loadApproximates()
+
+    println("Estimated Answer Error:")
+    printEstimatedErrors()
 
     println("Bias:")
     printBias()
@@ -186,31 +216,67 @@ class ErrorEstimationAccuracy() {
     println("Variance Error:")
     printVarianceError()
 
-    println("Confidence Interval Error:")
-    printConfidenceIntervalError()
+    Seq("diff", "abs-width", "width").foreach(method => {
+      println(s"Confidence Interval Error ($method):")
+      printConfidenceIntervalError(method)
+    })
 
-    println("Removing Samples ...")
-    removeSamples()
+    //    println("Removing Samples ...")
+    //    removeSamples()
+
+  }
+
+  def setDir() = {
+    val now = Calendar.getInstance()
+    dir = s"${now.get(Calendar.MONTH)}-${now.get(Calendar.DAY_OF_MONTH)}.${now.get(Calendar.HOUR_OF_DAY)}:${now.get(Calendar.MINUTE)}"
   }
 
   def createDirs(): Unit = {
     new File("error-test").mkdir()
+    setDir()
+    new File(s"error-test/$dir").mkdir()
+    val pw = new PrintWriter(new File(s"error-test/$dir/conf"))
+    pw.println(s"table = ${table}")
+    pw.println(s"nSamples = ${nSamples}")
+    pw.println(s"sampleSize = ${sampleSize}")
+    pw.println(s"confidence = ${conf.get("bootstrap.confidence")}")
+    pw.println(s"trials = ${conf.get("bootstrap.trials")}")
+    pw.println(s"method = ${conf.get("bootstrap.method")}")
+    pw.println(s"sampleType = ${conf.get("bootstrap.sample_type")}")
+    pw.close()
     queries.indices.foreach(i => {
-      new File(s"error-test/$i").mkdir()
-      val pw = new PrintWriter(new File(s"error-test/$i/q"))
+      new File(s"error-test/$dir/$i").mkdir()
+      val pw = new PrintWriter(new File(s"error-test/$dir/$i/q"))
       pw.println(queries(i))
       pw.close()
     })
   }
 }
 
+object ErrorEstimationAccuracy {
+  def run(): Unit = {
+    Seq(
+      (100, 95, "uda"),
+      (100, 95, "udf"),
+      (100, 95, "stored")
+    ).foreach(conf => {
+      val etest = new ErrorEstimationAccuracy()
+      etest.nSamples = 100
+      etest.conf.set("bootstrap.trials", conf._1 + "")
+      etest.conf.set("bootstrap.confidence", conf._2 + "%")
+      etest.conf.set("bootstrap.method", conf._3)
+      etest.run()
+    })
+  }
+}
+
 case class ApproxResult(value: Double, ciLow: Double, ciHigh: Double, error: Double, variance: Double)
+
 
 /*
 Shell commands:
 
 import edu.umich.verdict.expr._
-var etest = new ErrorEstimationAccuracy()
+ErrorEstimationAccuracy.run()
 
-
- */
+*/
