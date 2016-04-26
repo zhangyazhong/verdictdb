aba58c81a8fa90e5d37030fb5a7778e2644fd4d4
shahab <ahmadshahab.t@gmail.com>
2016-04-26 19:51:15 -0400

    AVG in UDF approach changed to SUM/n

--- src/main/java/edu/umich/verdict/transformation/UdfTransformer.java
+++ src/main/java/edu/umich/verdict/transformation/UdfTransformer.java
@@ -13,7 +13,7 @@ public class UdfTransformer extends QueryTransformer {
     protected String getTrialExpression(SelectListItem item, int trial) {
         switch (item.getAggregateType()) {
             case AVG:
-                return "sum((" + item.getInnerExpression() + ") * verdict.poisson(" + trial + "))/sum(verdict.poisson(" + trial + "))";
+                return "sum((" + item.getInnerExpression() + ") * verdict.poisson(" + trial + "))/" + transformed.getSample().getRowCount() + ")";
             case SUM:
                 return "sum((" + item.getInnerExpression() + ") * verdict.poisson(" + trial + "))";
             case COUNT:
