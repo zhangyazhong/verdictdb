2275741c6242010d37ff71b3cdcd5bf20c18f84e
Yongjoo Park <pyongjoo@umich.edu>
2017-11-26 00:26:45 -0500

    bug fix; lateral view throws an error when samples are present

--- core/src/test/java/edu/umich/verdict/relation/ParsingTest.java
+++ core/src/test/java/edu/umich/verdict/relation/ParsingTest.java
@@ -19,9 +19,28 @@ public class ParsingTest {
         
         String sql = "select * from mytable t lateral view explode(mycolumn_list) s as mycolumn";
         ExactRelation r = ExactRelation.from(vc, sql);
-        
         System.out.println(r.toSql());
-        
+    }
+    
+    @Test
+    public void test2() throws VerdictException {
+    	final String host = "salat1.eecs.umich.edu";
+        final String port = "21050";
+        final String schema = "gaddepa2";
+
+        VerdictConf conf = new VerdictConf();
+        conf.setDbms("impala");
+        conf.setHost(host);
+        conf.setPort(port);
+        conf.setDbmsSchema(schema);
+        conf.set("verdict.loglevel", "debug");
+        conf.set("verdict.meta_data.meta_database_suffix", "_verdict");
+        VerdictJDBCContext vc = VerdictJDBCContext.from(conf);
+    	
+    	String sql = "select count(*) from cbb_simple_yongjoo lateral view explode(multinomial_dept_list) t as multinomial_dept";
+//        ExactRelation r = ExactRelation.from(vc, sql);
+//        System.out.println(r.toSql());
+        vc.executeJdbcQuery(sql);
     }
 
 }
