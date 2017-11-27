5e0ea70d5d0c1c52d5c4f5796d70aa3aa9db174f
Yongjoo Park <pyongjoo@umich.edu>
2017-11-26 19:08:26 -0500

    column names in lateral view are properly resolved

--- core/src/test/java/edu/umich/verdict/relation/ParsingTest.java
+++ core/src/test/java/edu/umich/verdict/relation/ParsingTest.java
@@ -24,7 +24,7 @@ public class ParsingTest {
     
     @Test
     public void test2() throws VerdictException {
-    	final String host = "salat1.eecs.umich.edu";
+    	    final String host = "salat1.eecs.umich.edu";
         final String port = "21050";
         final String schema = "gaddepa2";
 
@@ -37,9 +37,40 @@ public class ParsingTest {
         conf.set("verdict.meta_data.meta_database_suffix", "_verdict");
         VerdictJDBCContext vc = VerdictJDBCContext.from(conf);
     	
-    	String sql = "select count(*) from cbb_simple_yongjoo lateral view explode(multinomial_dept_list) t as multinomial_dept";
-//        ExactRelation r = ExactRelation.from(vc, sql);
-//        System.out.println(r.toSql());
+    	    String sql = "select count(*) from cbb_simple_yongjoo lateral view explode(multinomial_dept_list) t as multinomial_dept";
+        vc.executeJdbcQuery(sql);
+    }
+    
+    @Test
+    public void test3() throws VerdictException {
+            final String host = "salat1.eecs.umich.edu";
+        final String port = "21050";
+        final String schema = "gaddepa2";
+
+        VerdictConf conf = new VerdictConf();
+        conf.setDbms("impala");
+        conf.setHost(host);
+        conf.setPort(port);
+        conf.setDbmsSchema(schema);
+        conf.set("verdict.loglevel", "debug");
+        conf.set("verdict.meta_data.meta_database_suffix", "_verdict");
+        VerdictJDBCContext vc = VerdictJDBCContext.from(conf);
+        
+        String sql = "select count(*) as totalcount\n" + 
+                "from \n" + 
+                "  (select xTable.*,\n" + 
+                "          Col1\n" + 
+                "   from\n" + 
+                "     (select pTable.*,\n" + 
+                "             dept AS Col2\n" + 
+                "      from\n" + 
+                "        (select *\n" + 
+                "         from cbb_simple_yongjoo\n" + 
+                "         where process_date = '9/28/2017') pTable LATERAL VIEW explode(multinomial_dept_list) deptT AS dept\n" + 
+                "     ) xTable\n" + 
+                "     LATERAL VIEW explode(frozen_segment_names) pdateT AS Col1\n" + 
+                "  ) segTable\n" + 
+                "where Col1 = 'seg'\n";
         vc.executeJdbcQuery(sql);
     }
 
